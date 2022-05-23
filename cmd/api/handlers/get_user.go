e5fcbebb5d959b0d49f4652d97b8fdef1a56e180
evpeople <hangcaihui@gmail.com>
2022-05-23 23:53:06 +0800

    ✨ feat(GetUser):

--- cmd/api/handlers/get_user.go
+++ cmd/api/handlers/get_user.go
@@ -3,19 +3,21 @@ package handlers
 import (
 	"context"
 
+	"github.com/bytedance/gopkg/util/logger"
 	"github.com/evpeople/douyin/cmd/api/rpc"
 	"github.com/evpeople/douyin/kitex_gen/user"
 	"github.com/evpeople/douyin/pkg/errno"
 	"github.com/gin-gonic/gin"
 )
 
-func QueryNote(c *gin.Context) {
+func GetUser(c *gin.Context) {
 	// claims := jwt.ExtractClaims(c)
 	// userID := int64(claims[constants.IdentityKey].(float64))
 	var queryVar struct {
 		UserID int64  `json:"user_id" form:"user_id"`
 		Token  string `json:"token" form:"token"`
 	}
+
 	if err := c.BindQuery(&queryVar); err != nil {
 		SendRegisterResponse(c, errno.ConvertErr(err), nil)
 	}
@@ -26,9 +28,10 @@ func QueryNote(c *gin.Context) {
 	// 	req.SearchKey = &queryVar.SearchKeyword
 	// }
 	user, err := rpc.GetUser(context.Background(), req)
+	logger.Debug("getUser", user)
 	// notes, total, err := rpc.QueryNotes(context.Background(), req)
 	if err != nil {
-		SendRegisterResponse(c, errno.ConvertErr(err), nil)
+		SendUserResponse(c, errno.ConvertErr(err), nil)
 		return
 	}
 	// SendResponse(c, errno.Success, map[string]interface{}{constants.Total: total, constants.Notes: notes})
