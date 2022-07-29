2bffbab887c455048d2dde81b23d1c4e2639af36
evpeople <hangcaihui@gmail.com>
2022-07-29 23:38:26 +0800

    🦄 refactor(main/handler): move AuthMiddle

--- cmd/api/handlers/handler.go
+++ cmd/api/handlers/handler.go
@@ -1,10 +1,14 @@
 package handlers
 
 import (
+	"context"
 	"net/http"
+	"time"
 
-	"github.com/evpeople/douyin/cmd/core/user/pack"
+	jwt "github.com/appleboy/gin-jwt/v2"
+	"github.com/evpeople/douyin/cmd/api/rpc"
 	"github.com/evpeople/douyin/kitex_gen/user"
+	"github.com/evpeople/douyin/pkg/constants"
 	"github.com/evpeople/douyin/pkg/errno"
 	"github.com/gin-gonic/gin"
 )
@@ -40,7 +44,6 @@ func SendBaseResponse(c *gin.Context, err error) {
 		Code:    Err.ErrCode,
 		Message: Err.ErrMsg,
 	})
-	return
 }
 
 type UserParam struct {
@@ -54,17 +57,73 @@ type UserResp struct {
 }
 
 type UserResponse struct {
-	// Code    int64  `json:"status_code"`
-	// Message string `json:"status_msg"`
-	// User  `json:"user_id"`
-	user.DouyinUesrMessageResponse
+	Code    int64  `json:"status_code"`
+	Message string `json:"status_msg"`
+	User    User   `json:"user"`
+}
+type User struct {
+	Id            int64  `protobuf:"varint,1,opt,name=id,proto3" json:"id"`
+	Name          string `protobuf:"bytes,2,opt,name=name,proto3" json:"name"`
+	FollowCount   *int64 `protobuf:"varint,3,opt,name=follow_count,json=followCount,proto3,oneof" json:"follow_count"`
+	FollowerCount *int64 `protobuf:"varint,4,opt,name=follower_count,json=followerCount,proto3,oneof" json:"follower_count"`
+	IsFollow      bool   `protobuf:"varint,5,opt,name=is_follow,json=isFollow,proto3" json:"is_follow"`
 }
 
 func SendUserResponse(c *gin.Context, err error, data *user.User) {
-	// Err := errno.ConvertErr(err)
-	var resp user.DouyinUesrMessageResponse
-	resp.BaseResponse = pack.BuildBaseResp(err)
-	resp.User = data
-	c.JSON(http.StatusOK,
-		resp)
+	Err := errno.ConvertErr(err)
+	c.JSON(http.StatusOK, UserResponse{
+		Code:    Err.ErrCode,
+		Message: Err.ErrMsg,
+		User: User{
+			Id:            data.Id,
+			Name:          data.Name,
+			FollowCount:   data.FollowCount,
+			FollowerCount: data.FollowerCount,
+			IsFollow:      data.IsFollow,
+		},
+	})
+}
+
+var AuthMiddleware *jwt.GinJWTMiddleware
+
+func init() {
+
+	AuthMiddleware, _ = jwt.New(&jwt.GinJWTMiddleware{
+		Key:        []byte(constants.SecretKey),
+		Timeout:    time.Hour,
+		MaxRefresh: time.Hour,
+		PayloadFunc: func(data interface{}) jwt.MapClaims {
+			if v, ok := data.(int64); ok {
+				return jwt.MapClaims{
+					constants.IdentityKey: v,
+				}
+			}
+			return jwt.MapClaims{}
+		},
+		Authenticator: func(c *gin.Context) (interface{}, error) {
+			var loginVar UserParam
+
+			loginVar.UserName = c.Query("username")
+			loginVar.PassWord = c.Query("password")
+			if len(loginVar.UserName) == 0 || len(loginVar.PassWord) == 0 {
+				return "", jwt.ErrMissingLoginValues
+			}
+			id, err := rpc.CheckUser(context.Background(), &user.DouyinUserRequest{Username: loginVar.UserName, Password: loginVar.PassWord})
+			c.Set("userID", id)
+			return id, err
+		},
+		TokenLookup: "header: Authorization, query: token, cookie: jwt",
+		// TokenLookup:   "cookie: token",
+		TokenHeadName: "Bearer",
+		TimeFunc:      time.Now,
+		LoginResponse: func(c *gin.Context, code int, message string, time time.Time) {
+			id := c.GetInt64("userID")
+			c.JSON(http.StatusOK, RegisterResponse{
+				Code:    int64(code),
+				Message: errno.Success.ErrMsg,
+				UserID:  id,
+				Token:   message,
+			})
+		},
+	})
 }
--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,17 +1,12 @@
 package main
 
 import (
-	"context"
 	"net/http"
-	"time"
 
-	jwt "github.com/appleboy/gin-jwt/v2"
 	"github.com/cloudwego/kitex/pkg/klog"
 	"github.com/evpeople/douyin/cmd/api/handlers"
 	"github.com/evpeople/douyin/cmd/api/rpc"
-	"github.com/evpeople/douyin/kitex_gen/user"
 	"github.com/evpeople/douyin/pkg/constants"
-	"github.com/evpeople/douyin/pkg/errno"
 	"github.com/evpeople/douyin/pkg/tracer"
 	"github.com/gin-gonic/gin"
 )
@@ -24,56 +19,21 @@ func Init() {
 func main() {
 	Init()
 	r := gin.New()
-	authMiddleware, _ := jwt.New(&jwt.GinJWTMiddleware{
-		Key:        []byte(constants.SecretKey),
-		Timeout:    time.Hour,
-		MaxRefresh: time.Hour,
-		PayloadFunc: func(data interface{}) jwt.MapClaims {
-			if v, ok := data.(int64); ok {
-				return jwt.MapClaims{
-					constants.IdentityKey: v,
-				}
-			}
-			return jwt.MapClaims{}
-		},
-		Authenticator: func(c *gin.Context) (interface{}, error) {
-			var loginVar handlers.UserParam
-			if err := c.ShouldBind(&loginVar); err != nil {
-				return "", jwt.ErrMissingLoginValues
-			}
-
-			if len(loginVar.UserName) == 0 || len(loginVar.PassWord) == 0 {
-				return "", jwt.ErrMissingLoginValues
-			}
-			id, err := rpc.CheckUser(context.Background(), &user.DouyinUserRequest{Username: loginVar.UserName, Password: loginVar.PassWord})
-			c.Set("userID", id)
-			return id, err
-		},
-		TokenLookup:   "header: Authorization, query: token, cookie: jwt",
-		TokenHeadName: "Bearer",
-		TimeFunc:      time.Now,
-		LoginResponse: func(c *gin.Context, code int, message string, time time.Time) {
-			// token := jwt.ExtractClaimsFromToken(jwt.GetToken(c))
-			id := c.GetInt64("userID")
-			c.JSON(http.StatusOK, handlers.RegisterResponse{
-				Code:    int64(code),
-				Message: errno.Success.ErrMsg,
-				UserID:  id,
-				Token:   message,
-			})
-			// logger.Debug(c.Get("JWT_PAYLOAD"))
-		},
-	})
 
 	v1 := r.Group("/douyin")
-	v1.Use(authMiddleware.MiddlewareFunc())
 	user1 := v1.Group("/user")
 	user1.GET("", handlers.GetUser)
 	// authMiddl
-	user1.POST("/login", authMiddleware.LoginHandler)
+	user1.POST("/login", handlers.AuthMiddleware.LoginHandler)
 	user1.POST("/register", handlers.Register)
 	feed := v1.Group("/feed")
+	feed.Use(handlers.AuthMiddleware.MiddlewareFunc())
 	feed.GET("", handlers.GetVideos)
+	publish := v1.Group("/publish")
+	//TODO:添加中间件把Token放到header中
+	publish.Use(handlers.AuthMiddleware.MiddlewareFunc())
+	// publish.GET("/list",handler.)
+	publish.POST("/action", handlers.UploadVideo)
 	if err := http.ListenAndServe(":8080", r); err != nil {
 		klog.Fatal(err)
 	}
