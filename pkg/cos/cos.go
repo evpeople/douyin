e3dac85a8cdf36f576eb09b07ab319bb02ffa8c5
evpeople <hangcaihui@gmail.com>
2022-07-31 16:16:06 +0800

    🦄 refactor(pkg/cos): add snapshot

--- pkg/cos/cos.go
+++ pkg/cos/cos.go
@@ -3,6 +3,7 @@ package cos
 import (
 	"context"
 	"io"
+	"log"
 	"net/http"
 	"net/url"
 	"os"
@@ -10,16 +11,44 @@ import (
 	"github.com/tencentyun/cos-go-sdk-v5"
 )
 
-// var DefaultClient *cos.Client
-
-var DefaultClient = &cos.Client{}
+var DefaultVideoClient *cos.Client
+var DefaultPicClient *cos.Client
 
 func init() {
+	DefaultVideoClient = createClient("BACKETVIDEOURL")
+	DefaultPicClient = createClient("BACKETPICURL")
+}
+func UploadVideo(key string, r io.Reader) (videoUrl string, picUrl string, err error) {
+	optPut := &cos.ObjectPutOptions{}
+	_, err = DefaultVideoClient.Object.Put(context.Background(), key, r, optPut)
+	if err != nil {
+		log.Println(err)
+	}
+	respVideo := DefaultVideoClient.Object.GetObjectURL(key)
+
+	optSnap := &cos.GetSnapshotOptions{
+		Time: 1,
+	}
+	PicResp, err := DefaultVideoClient.CI.GetSnapshot(context.Background(), key, optSnap)
+	if err != nil {
+		log.Println(err)
+	}
+	_, err = DefaultPicClient.Object.Put(context.Background(), key, PicResp.Body, optPut)
+	if err != nil {
+		log.Println(err)
+	}
+	respPic := DefaultPicClient.Object.GetObjectURL(key)
+	return respVideo.String(), respPic.String(), err
+}
+func createClient(backetUrl string) *cos.Client {
+
 	// 存储桶名称，由bucketname-appid 组成，appid必须填入，可以在COS控制台查看存储桶名称。 https://console.cloud.tencent.com/cos5/bucket
 	// 替换为用户的 region，存储桶region可以在COS控制台“存储桶概览”查看 https://console.cloud.tencent.com/ ，关于地域的详情见 https://cloud.tencent.com/document/product/436/6224 。
-	u, _ := url.Parse(os.Getenv("BACKETURL"))
-	b := &cos.BaseURL{BucketURL: u}
-	DefaultClient = cos.NewClient(b, &http.Client{
+	u, err := url.Parse(os.Getenv(backetUrl))
+	if err != nil {
+		panic(err)
+	}
+	return cos.NewClient(&cos.BaseURL{BucketURL: u}, &http.Client{
 		Transport: &cos.AuthorizationTransport{
 			// 通过环境变量获取密钥
 			// 环境变量 SECRETID 表示用户的 SecretId，登录访问管理控制台查看密钥，https://console.cloud.tencent.com/cam/capi
@@ -28,11 +57,4 @@ func init() {
 			SecretKey: os.Getenv("SECRETKEY"),
 		},
 	})
-
-}
-func UploadVideo(key string, r io.Reader) (string, error) {
-	opt := &cos.ObjectPutOptions{}
-	_, err := DefaultClient.Object.Put(context.Background(), key, r, opt)
-	resp := DefaultClient.Object.GetObjectURL(key)
-	return resp.String(), err
 }
